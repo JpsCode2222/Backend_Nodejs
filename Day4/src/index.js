28fe17957932c2a67e850816831f8a22541b1949
jayad <jp@gmail.com>
2024-01-16 16:17:07 +0530

    database Successfully connected

--- Day4/src/index.js
+++ Day4/src/index.js
@@ -1,1 +1,9 @@
-import express from "express";
+import mongoose from "mongoose";
+import connectDB from "./db/index.js";
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./env",
+});
+connectDB();
+const port = process.env.PORT;
