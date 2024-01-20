0d961e98dbb274530e269981b3b3d7df468f0533
jayad <jp@gmail.com>
2024-01-20 12:10:37 +0530

    fix login logout

--- Day4/src/routes/user.routes.js
+++ Day4/src/routes/user.routes.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import {
+  loggoutUser,
+  loginUser,
+  registerUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -17,5 +23,10 @@ router.route("/register").post(
   ]),
   registerUser
 );
+
+router.route("/login").post(loginUser);
+
+// Secured Routes
+router.route("/logout").post(verifyJWT, loggoutUser);
 
 export default router;
